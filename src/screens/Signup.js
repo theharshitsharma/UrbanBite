f6268a3f4d318a119e9c139aed9f0cf1e68b3adc
theharshitsharma <harshit2004ji@gmail.com>
2025-07-05 19:59:28 +0530

    signup issue fix

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -76,7 +76,7 @@ function Signup() {
 
   const completeSignup = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/createuser", {
+      const response = await fetch("https://urbanbite-backend.onrender.com/api/createuser", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(credentials)
